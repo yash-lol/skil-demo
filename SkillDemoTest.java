5285abea25338caacccade5422b585274c47741d
yash-lol <yjoshi@ucsd.edu>
2022-02-04 13:14:31 -0800

    added lib + failed test

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(5, SkillDemo.add(2, 4)); // failed test
+
+    }
+}
